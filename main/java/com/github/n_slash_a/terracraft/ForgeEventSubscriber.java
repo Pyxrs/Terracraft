53052f09c466408469bf8fdcb738ff8f6e9c7c34
N_slash_A <cyrus.steiner@gmail.com>
2019-10-12 08:22:40 -0600

    Revert "Revert "Updated Forge""
    This reverts commit 723cecce6952d1f579c32bc12bd9485aad3b5fe8.

--- main/java/com/github/n_slash_a/terracraft/ForgeEventSubscriber.java
+++ main/java/com/github/n_slash_a/terracraft/ForgeEventSubscriber.java
@@ -1,12 +1,5 @@
 package com.github.n_slash_a.terracraft;
 
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-
-/**
- * Subscribe to events from the FORGE EventBus that should be handled on both PHYSICAL sides in this class
- *
- * @author Cadiboo
- */
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public final class ForgeEventSubscriber {
 
