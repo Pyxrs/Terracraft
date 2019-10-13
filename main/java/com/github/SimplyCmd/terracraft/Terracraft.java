eddd20c5ff7c5528407ec926c88cc974c11d467b
N_slash_A <cyrus.steiner@gmail.com>
2019-10-13 15:58:32 -0600

    Adding support for custom crops

--- main/java/com/github/SimplyCmd/terracraft/Terracraft.java
+++ main/java/com/github/SimplyCmd/terracraft/Terracraft.java
@@ -2,17 +2,38 @@ package com.github.SimplyCmd.terracraft;
 
 import static com.github.SimplyCmd.terracraft.Terracraft.MOD_ID;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 /**
  * The main class of the mod, this is the class that looks like a mod to forge.
  */
 @Mod(MOD_ID)
 public class Terracraft {
-
     /**
      * The modid of this mod, this has to match the modid in the mods.toml and has to be in the format defined in {@link net.minecraftforge.fml.loading.moddiscovery.ModInfo}
      */
     public static final String MOD_ID = "terracraft";
+    public static Terracraft instance;
+    private static final Logger logger = LogManager.getLogger(MOD_ID);
 
+    public Terracraft() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        logger.info("Setup method registered");
+    }
+    private void clientRegistries(final FMLClientSetupEvent event) {
+        logger.info("clientRegistries method registered");
+    }
 }
