58cf768bc6e1248cb5f7a2f1fa369fac72d12069
N_slash_A <cyrus.steiner@gmail.com>
2020-04-03 17:29:04 -0600

    Partially added 100 Items
    - Added comments to explain what is happening
    - Fixed update checker
    - Fixed some spacing issues in the mod info
    - Added 100 items

--- main/java/io/github/simplycmd/terracraft/Main.java
+++ main/java/io/github/simplycmd/terracraft/Main.java
@@ -6,12 +6,14 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(Main.MODID)
 public class Main {
+    // The MODID of the mod
     public static final String MODID = "terracraft";
 
+    // A logger to print messages into the console
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
+    // This runs on mod startup
     public Main() {
         LOGGER.info("Hello! This is Terracraft. Just to let you know, I am setting up a few things right now. In a bit of time, you should be able to start playing Minecraft. But, for now, you're stuck with me. I have so many questions, just waiting to be answered! How are you today? Do you like cheese? If so, which kind? Ok ima go now BYEEEEEEEEEE!");
-
     }
 }
