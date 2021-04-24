a401ecea9c50510bc8d69a06ae0d9950945510c2
SimplyCmd <simplycmd00@gmail.com>
2021-04-23 21:46:38 -0600

    dart trap prototype & update to latest snapshot

--- src/main/java/io/github/simplycmd/terracraft/Blocks.java
+++ src/main/java/io/github/simplycmd/terracraft/Blocks.java
@@ -1,5 +1,8 @@
 package io.github.simplycmd.terracraft;
 
+import io.github.simplycmd.terracraft.blocks.DartTrapBlock;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -9,12 +12,24 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 public class Blocks {
+    public static Block HELLSTONE_BLOCK;
 
     public static void RegisterBlocks() {
-        RegisterBlock("hellstone_block", new Block(FabricBlockSettings.of(Material.METAL).strength(1.5f, 6.0f).requiresTool()), ItemGroup.BUILDING_BLOCKS);
+        HELLSTONE_BLOCK = RegisterBlock("hellstone_block", new Block(FabricBlockSettings.of(Material.METAL).strength(1.5f, 6.0f).requiresTool()), ItemGroup.BUILDING_BLOCKS);
+        RegisterOverworldOre("hellstone_ore", HELLSTONE_BLOCK, YOffset.getBottom(), YOffset.getTop(), 9, 20);
+
+        RegisterBlock("dart_trap", new DartTrapBlock(FabricBlockSettings.of(Material.STONE).strength(1.5f, 6.0f).requiresTool()), ItemGroup.DECORATIONS);
     }
 
     public static Block RegisterBlock(String id, Block block, ItemGroup group) {
@@ -23,8 +38,23 @@ public class Blocks {
         return block;
     }
 
-    public static ConfiguredFeature<?, ?> RegisterOre(String id, ConfiguredFeature<?, ?> feature) {
-        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(Main.MOD_ID, id), feature);
-        return feature;
+    public static ConfiguredFeature<?, ?> RegisterOverworldOre(String id, Block block, YOffset lowest, YOffset highest, Integer vein_size, Integer veins_per_chunk) {
+        ConfiguredFeature<?, ?> ORE = Feature.ORE
+                .configure(new OreFeatureConfig(
+                        OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                        block.getDefaultState(),
+                        vein_size))
+                .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(lowest, highest))))
+                .spreadHorizontally()
+                .repeat(veins_per_chunk);
+
+        RegistryKey<ConfiguredFeature<?, ?>> oreWoolOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
+                new Identifier("tutorial", "ore_wool_overworld"));
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), ORE);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreWoolOverworld);
+
+        return ORE;
     }
+
+
 }
---
+++ src/main/java/io/github/simplycmd/terracraft/blocks/DartTrapBlock.java
@@ -0,0 +1,49 @@
+package io.github.simplycmd.terracraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.*;
+import net.minecraft.world.World;
+
+public class DartTrapBlock extends HorizontalFacingBlock {
+
+    public DartTrapBlock(Settings settings) {
+        super(settings);
+        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(Properties.HORIZONTAL_FACING);
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
+        if (!world.isClient) {
+            if (world.isReceivingRedstonePower(pos)) {
+                ArrowEntity arrowEntity = new ArrowEntity(world, pos.getX(), pos.getY(), pos.getZ());
+                arrowEntity.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+
+                Vec3d arrow_pos = arrowPosition(pos, state);
+                arrowEntity.updatePosition(arrow_pos.getX(), arrow_pos.getY(), arrow_pos.getZ());
+                arrowEntity.updateVelocity(1, Vec3d.of(state.get(Properties.HORIZONTAL_FACING).getVector()));
+                world.spawnEntity(arrowEntity);
+            }
+        }
+    }
+
+    private Vec3d arrowPosition(BlockPos pos, BlockState state) {
+        Vec3i vec = state.get(Properties.HORIZONTAL_FACING).getVector();
+        return new Vec3d(pos.getX() + vec.getX() + 0.5, pos.getY() + vec.getY() + 0.5, pos.getZ() + vec.getZ() + 0.5);
+    }
+
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+    }
+}
